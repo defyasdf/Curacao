a45b5ed84572c0d6e4b95a5c5be5ff57205e1c19
snprajapati <sanjay.prajapati@icuracao.com>
2013-06-11 18:39:49 -0700

    Add Cat_tree

--- custom/_getorder_item_collection_margin.php
+++ custom/_getorder_item_collection_margin.php
@@ -4,6 +4,21 @@
 	ini_set('display_errors', 1);
 	ini_set("memory_limit","1024M");
 	
+	$server = '192.168.100.121';
+	$user = 'curacaodata';
+	$pass = 'curacaodata';
+	$db = 'curacao_magento';
+	
+	
+	
+	$link = mysql_connect($server,$user,$pass);
+	$link1 = mysql_connect($server,$user,$pass,true);
+	
+	mysql_select_db($db,$link) or die("No DB");	
+	mysql_select_db('icuracaoproduct',$link1) or die("No DB");	
+
+
+	
 	$mageFilename = '../app/Mage.php';
 	
 	require_once $mageFilename;
@@ -13,7 +28,24 @@
 
 	umask(0);
 	Mage::app('admin'); 
+	$cat = Mage::getModel('catalog/category')->load(466);
+	$subcatcollection = $cat->getChildren();
 	
+	$subcat = explode(",",$subcatcollection);
+	
+	$cat_collecton = $subcat;
+	$final_list = array();
+	for($i=0;$i<sizeof($cat_collecton);$i++){
+		$scat = $cat = Mage::getModel('catalog/category')->load($cat_collecton[$i]);
+		$scatcollection = $scat->getChildren();
+		if(trim($scatcollection)!=''){
+			$sucat = explode(",",$scatcollection);
+			$final_list = array_merge($cat_collecton,$sucat);
+		}
+	}
+	if(sizeof($final_list)==0){
+		$final_list = $cat_collecton;
+	}
 	$string = '';
 	function getcattree($catId){
 		global $string;
@@ -80,13 +112,28 @@
 		}else{
 			$percent = '';
 		}
-		$cat = $product->getCategoryIds();
+		/*$cat = $product->getCategoryIds();
 		//$cat_ids = implode('_',$cat);
 		
 		$cName = array();
 		for($i = 0;$i<(sizeof($cat)-1);$i++){
 			$cName[] = Mage::getModel('catalog/category')->load($cat[$i])->getName();
-		}
+		}*/
+		
+		$sql = "SELECT cc . * FROM catalog_category_entity cc JOIN catalog_category_product cp ON cc.entity_id = cp.category_id WHERE cp.product_id = ".$product->getId()." AND cc.path NOT LIKE '1/2/466%' ORDER BY `cc`.`level` DESC";
+			$result = mysql_query($sql,$link);
+			$row = mysql_fetch_array($result);
+			
+			$cat_id = str_replace('1/2/','',$row['path']);
+			$cattree = explode('/',$cat_id);
+			$cName = array();
+			
+			for($j=0;$j<sizeof($cattree);$j++){
+				if(!in_array($cattree[$j],$final_list)){
+					$cName[] = Mage::getModel('catalog/category')->load($cattree[$j])->getName();
+				}
+			}
+		
 		$cat_ids = implode('_',$cName);	
 		
 		$data[] = array("Order_id"=>$order->getId(),"Order Number"=>$order->getIncrement_id(),"Custmer_number"=>$order->getCuracaocustomernumber(),'AR_Estimate'=>$order->getEstimatenumber(), "State"=>$order->getState(), "Status"=>$order->getStatus(), "Store"=>$store, "Order_date"=>$order->getCreated_at(),"Units_per_product"=>$item->getQty_ordered(),'sku'=>$item->getSku(),'Name'=>$item->getName(),'UNIT_PRICE'=>$item->getPrice(),"Cost_Price"=>$product->getCost(),"Margin"=>$margin,"Percent_Margin"=>$percent.' %', "Category_Ids"=>$cat_ids);
