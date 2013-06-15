98ff57c0eff56212a9bb57d42439ca741b5c9e3b
snprajapati <sanjay.prajapati@icuracao.com>
2013-06-14 18:33:40 -0700

    Change the Flate rate file for free shipping

--- custom/getrules.php
+++ custom/getrules.php
@@ -23,11 +23,16 @@
 
         $conditions = $rule->getConditions()->asArray();
 		
-		print_r($conditions);
 		
-		echo $conditions['conditions'][0]['conditions'][0]['value'];
-			
+		$condistion = $conditions['conditions'][0]['conditions'];
 		
+		$cat = array();
+		echo sizeof($cat);	
+		for($i = 0; $i<sizeof($condistion);$i++){
+			$cat[] = $condistion[$i]['value'];
+		}
+		print_r($cat);
+		exit;
 		if($rule->getIsActive()){
 			$start_ts = strtotime($rule->getfrom_date());
 			$end_ts = strtotime($rule->getto_date());
