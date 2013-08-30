e3da2f20b20abbf8e61fb37b17dd3ab9b731dd47
snprajapati <sanjay.prajapati@icuracao.com>
2013-08-30 13:06:12 -0700

    overriding cache

---
+++ upgrade/app/code/local/Magify/Page/Model/Observer.php
@@ -0,0 +1,27 @@
+<?php
+class Magify_Page_Model_Observer
+{
+	public function processPreDispatch(Varien_Event_Observer $observer)
+	{
+		$page = Mage::helper('core/url')->getCurrentUrl() ;
+		$base = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+		
+		$current_page = str_replace($base,'',$page);
+		
+		if($current_page == 'credit-application' || $current_page == 'preapprove'){
+	
+			$action = $observer->getEvent()->getControllerAction();
+		
+			// Check to see if $action is a Product controller
+			//if ($action instanceof Mage_Catalog_ProductController)
+			//{
+				$request = $action->getRequest();
+				$cache = Mage::app()->getCacheInstance();
+		
+				// Tell Magento to 'ban' the use of FPC for this request
+				$cache->banUse('full_page');
+			//}
+		}
+		
+	}
+}
