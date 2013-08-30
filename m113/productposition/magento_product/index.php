60fa004ebfcaf52b3ba0971937d7ad3daf83a97e
snprajapati <sanjay.prajapati@icuracao.com>
2013-08-30 10:06:39 -0700

    Updated Drag and Drop feature

---
+++ m113/productposition/magento_product/index.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('apc.cache_by_default','Off');
+/**
+ * Main Index File.
+ * 
+ * App is single point entry
+ * 
+ * Assigns constant vars
+ * Loads the loader
+ * 
+ * 
+ * @version 1.0
+ * @package MagentoProductOrder 
+ * @since May 28, 2013
+ * 
+ */
+
+
+//session_start();
+
+//die("D");
+
+include "init.php";
+include "loader.php";
+?>
