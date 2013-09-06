40e86090e35877fa45975c7011878c9c0e4d0aa4
snprajapati <sanjay.prajapati@icuracao.com>
2013-09-05 17:18:03 -0700

    Added the signup and get 10% off code

---
+++ staging/custom/emailsignup.php
@@ -0,0 +1,45 @@
+<?php
+	ini_set('max_execution_time', 0);
+	ini_set('display_errors', 1);
+	ini_set("memory_limit","1024M");
+	
+	$mageFilename = '../app/Mage.php';
+	
+	require_once $mageFilename;
+	Varien_Profiler::enable();
+	Mage::setIsDeveloperMode(true);
+
+
+	umask(0);
+	Mage::app('default'); 
+	
+	
+	$currentStore = Mage::app()->getStore()->getId();
+	Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+			
+	$email = $_REQUEST['email'];
+	
+	// THE "EASY" WAY (but sends a confirmation email to the customer
+	//$subscriber = Mage::getModel('newsletter/subscriber')->subscribe($email);
+	
+	
+	
+		$subscriber = Mage::getModel('newsletter/subscriber')->loadByEmail($email);
+			
+		if (!$subscriber->getId()) {
+			$subscriber->setStatus(Mage_Newsletter_Model_Subscriber::STATUS_SUBSCRIBED);
+		   	$subscriber->setSubscriberEmail($email);
+			//$subscriber->setSubscriberConfirmCode($subscriber->RandomSequence());
+			$subscriber->setStoreId(Mage::app()->getStore()->getId());
+			try {
+				$subscriber->save();
+				echo 'saved';
+			}
+			catch (Exception $e) {
+				throw new Exception($e->getMessage());
+			}
+			
+		}else{
+			echo 'Already Exist';
+		}
+		
